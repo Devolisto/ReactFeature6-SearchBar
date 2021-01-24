267060c1506ee9580d6fb3e0c2c9ae5a7f7a106e
devolisto <devolisto@gmail.com>
2021-01-24 15:12:30 +0300

    ...

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,16 +5,28 @@ import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
+// State
+import {useState} from "react";
 
 const ProductList = () => {
-  const productList = products.map((product) => (
+  const [query, setQuery] = useState();
+
+  const filteredList = products.filter((product) => product.name.inclueds(query))
+  console.log(filteredList);
+
+  filteredList.map((product) => (
     <ProductItem product={product} key={product.id} />
-  ));
+  ))
+  // const productList = products.map((product) => (
+  //   <ProductItem product={product} key={product.id} />
+  // ));
+
+  
 
   return (
     <>
-      <SearchBar />
-      <ListWrapper>{productList}</ListWrapper>
+      <SearchBar setQuery={setQuery} />
+      <ListWrapper>{filteredList}</ListWrapper>
     </>
   );
 };
